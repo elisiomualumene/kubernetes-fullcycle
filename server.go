592dc9db9a13baa45b4f1d16f52f8a879b34e9b8
Elísio Mualumene <85412882+elisiomualumene@users.noreply.github.com>
2023-08-04 14:32:16 +0000

    :fire: updated for another route

--- server.go
+++ server.go
@@ -1,12 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main(){
-	http.HandleFunc("/", Hello)
+	http.HandleFunc("/config", ConfigMap)
+	http.HandleFunc("/", ConfigMap)
 	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("<h1>Hello FullCycle!!</h1>"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request){
+	data, err :=  ioutil.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("error reading file", err)
+	}
+
+	fmt.Fprintf(w, "My family:%s", string(data))
 }
