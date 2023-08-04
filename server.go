5ef302a268e69533edde818a8be13959b3a4d35a
Elísio Mualumene <85412882+elisiomualumene@users.noreply.github.com>
2023-08-04 16:23:52 +0000

    :car: working with secrets

--- server.go
+++ server.go
@@ -10,16 +10,18 @@ import (
 
 func main(){
 	http.HandleFunc("/config", ConfigMap)
-	http.HandleFunc("/", ConfigMap)
+	http.HandleFunc("/", Secret)
+//	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8000", nil)
 }
 
-func Hello(w http.ResponseWriter, r *http.Request){
+/*func Hello(w http.ResponseWriter, r *http.Request){
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s", name, age)
 }
+*/
 
 func ConfigMap(w http.ResponseWriter, r *http.Request){
 	data, err :=  ioutil.ReadFile("myfamily/family.txt")
@@ -28,4 +30,11 @@ func ConfigMap(w http.ResponseWriter, r *http.Request){
 	}
 
 	fmt.Fprintf(w, "My family:%s", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request){
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(w, "User: %s. Password: %s", user, password)
 }
